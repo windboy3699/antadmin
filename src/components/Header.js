48ce4b7b2c502364f6b13e536bec2a48f56da351
PanXd <windboy3699@qq.com>
2021-03-09 15:25:08 +0800

    update header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import '../cover.css';
+
+const { SubMenu } = Menu;
 
 class Header extends Component {
+
+    state = {
+        current: 'mail',
+    };
+
+    handleClick = e => {
+        console.log('click ', e);
+        this.setState({ current: e.key });
+    };
+
     render() {
+        const { current } = this.state;
         return (
             <div className="header-wrap">
-                <ul className="header-nav">
-                    <li>首页</li>
-                    <li>用户中心</li>
-                    <li>财务系统</li>
-                </ul>
+                <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+                    <Menu.Item key="mail" icon={<MailOutlined />} className="xxxdddyy">
+                    Navigation One
+                    </Menu.Item>
+                    <Menu.Item key="appstore" icon={<AppstoreOutlined />}>
+                        Navigation Two
+                    </Menu.Item>
+                    <Menu.Item key="setting" icon={<SettingOutlined />}>
+                        Navigation Three
+                    </Menu.Item>
+                </Menu>
             </div>
         )
     }
