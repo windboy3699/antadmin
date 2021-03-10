9effb2159625c5213043e42901968327182a5fc3
PanXd <windboy3699@qq.com>
2021-03-10 17:13:55 +0800

    remove header left

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
 import './App.css';
-import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Header from "./components/Header";
-import Left from "./components/Left";
-import Main from "./components/Main";
-import List from "./components/List";
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import List from './components/List';
 
-class App extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-          <div>
-            <Header></Header>
-            <Left></Left>
-            <Main>
-              <Switch>
-                <Redirect exact from="/" to="/home/all" />
-                <Route path='/home/:tab' component={List}></Route>
-              </Switch>
-            </Main>
-          </div>
-        </BrowserRouter>
-    )
-  }
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+    state = {
+        collapsed: false,
+    };
+
+    onCollapse = collapsed => {
+        console.log(collapsed);
+        this.setState({ collapsed });
+    };
+
+    render() {
+        const { collapsed } = this.state;
+        return (
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+                    <div className="logo" />
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <Menu.Item key="1" icon={<PieChartOutlined />}>
+                            Option 1
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<DesktopOutlined />}>
+                            Option 2
+                        </Menu.Item>
+                        <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                            <Menu.Item key="3">Tom</Menu.Item>
+                            <Menu.Item key="4">Bill</Menu.Item>
+                            <Menu.Item key="5">Alex</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                            <Menu.Item key="6">Team 1</Menu.Item>
+                            <Menu.Item key="8">Team 2</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="9" icon={<FileOutlined />}>
+                            Files
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }} />
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                            <BrowserRouter>
+                                <Switch>
+                                    <Redirect exact from="/" to="/home/all" />
+                                    <Route path='/home/:tab' component={List}></Route>
+                                </Switch>
+                                {this.props.children}
+                            </BrowserRouter>
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
+            </Layout>
+        );
+    }
 }
 
 export default App;
